eb0b21b3788327ed08de6e55e14845afeba15d6a
Christian Lazo <lazochristian.cl@gmail.com>
2019-02-02 21:12:04 -0800

    Add mail form gem

---
+++ app/models/contact_form.rb
@@ -0,0 +1,14 @@
+class ContactForm < MailForm::Base
+  attributes :name,  validate: true
+  attributes :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attributes :message, validate: true
+  attributes :nickname,   :captcha => true
+
+  def headers
+    {
+      subject: 'My Contact Form',
+      to: 'lazochristian.cl@gmail.com',
+      from: %("#{name}" <#{email}>)
+    }
+  end
+end
