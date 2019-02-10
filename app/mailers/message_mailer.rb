87d5ccdaecedb4fa5bbd95e6f3945434647bdbbf
Christian Lazo <lazochristian.cl@gmail.com>
2019-02-10 15:54:28 -0800

    Updates to mail gun

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,6 +1,4 @@
 class MessageMailer < ApplicationMailer
-  require 'mail_gun'
-
   def contact(message)
     @body = message.body
     mg_client = Mailgun::Client.new(ENV["MAILGUN_API_KEY"])
@@ -8,6 +6,6 @@ class MessageMailer < ApplicationMailer
                       :to => ENV['email'],
                       :subject => 'Contact Form',
                       :text => message.body}
-    mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
+    mg_client.send_message(ENV['MAILGUN_DOMAIN'], message_params)
   end
 end
